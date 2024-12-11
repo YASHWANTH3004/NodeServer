5050f43831fffd455fd2d5deff77ed6f53d5129f
YASHWANTH3004 <n.yashu30@gmail.com>
2024-12-11 23:57:26 +0530

    Redirection

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Enter Form Details</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><input type='submit' value='submit'></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("Hello.txt", "Hello");
+    res.setHeader("Location", "/");
+    res.statusCode = 302;
+    return res.end();
+  }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>Basic Server</title></head>");
@@ -8,5 +29,4 @@ const server = http.createServer((req, res) => {
   res.write("</html>");
   res.end();
 });
-
 server.listen(3000);
