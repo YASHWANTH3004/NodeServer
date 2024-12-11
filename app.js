528873f9ec37483e0537ec4d59e8697fabcc5291
YASHWANTH3004 <n.yashu30@gmail.com>
2024-12-12 00:21:56 +0530

    Added route file

--- app.js
+++ app.js
@@ -1,45 +1,5 @@
 const http = require("http");
-const fs = require("fs");
+const requestHandler = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Enter Form Details</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><input type='submit' value='submit'></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log(chunk);
-    });
-
-    return req.on("end", () => {
-      console.log("End event received");
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=");
-      fs.writeFile("Hello.txt", message[1], (err) => {
-        console.log("Filewrite completed!");
-        res.setHeader("Location", "/");
-        res.statusCode = 302;
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Basic Server</title></head>");
-  res.write("<body><h1>Welcome to my server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(requestHandler);
 server.listen(3000);
